709ce1d617d44e0c13b7fc13fd62c60f980b31e0
goliatone <burgosemi@gmail.com>
2014-10-02 18:03:15 -0400

    Add shell: methods and attributes

--- src/windowsocket.js
+++ src/windowsocket.js
@@ -77,7 +77,20 @@
     ///////////////////////////////////////////////////
 
     var OPTIONS = {
-        autoinitialize: true
+        autoinitialize: true,
+
+        binaryType: 'blob',
+        bufferedAmount: 0,
+        extensions: '',
+
+        onerror: function() {},
+        onclose: function() {},
+        onmessage: function() {},
+        onopen: function() {},
+
+        protocol: '',
+        readyState: 0,
+        url: ''
     };
 
     /**
@@ -101,11 +114,51 @@
      */
     WindowSocket.DEFAULTS = OPTIONS;
 
+    /**
+     * The connection is not yet open.
+     */
+    WindowSocket.CONNECTING = 0
+    /**
+     * The connection is open and ready to communicate.
+     */
+    WindowSocket.OPEN = 1
+    /**
+     * The connection is in the process of closing.
+     */
+    WindowSocket.CLOSING = 2
+    /**
+     * The connection is closed or couldn't be opened.
+     */
+    WindowSocket.CLOSED = 3
+
     ///////////////////////////////////////////////////
     // PRIVATE METHODS
     ///////////////////////////////////////////////////
 
-    WindowSocket.prototype.init = function(config) {
+    /**
+     * The WindowSocket object provides the API for creating
+     * and managing a WindowSocket connection to a server,
+     * as well as for sending and receiving data
+     * on the connection.
+     *
+     * @param  {String} url       The URL to which to connect; this
+     *                            should be the URL to which the
+     *                            WindowSocket
+     *                            server will respond.
+     * @param  {String|Array} protocols Optional. Either a single protocol
+     *                                  string or an array of protocol strings.
+     *                                  These strings are used to indicate
+     *                                  sub-protocols, so that a single server
+     *                                  can implement multiple WebSocket
+     *                                  sub-protocols (for example, you might
+     *                                  want one server to be able to handle
+     *                                  different types of interactions depending
+     *                                  on the specified protocol). If you don't
+     *                                  specify a protocol string, an empty string
+     *                                  is assumed.
+     * @throws SECURITY_ERR The port to which the connection is being attempted is being blocked.
+     */
+    WindowSocket.prototype.init = function(url, protocols, config) {
         if (this.initialized) return this.logger.warn('Already initialized');
         this.initialized = true;
 
@@ -113,6 +166,46 @@
         _extend(this, config);
 
         return 'This is just a stub!';
+    };
+
+    /**
+     * Transmits data to the server over the WindowSocket
+     * connection.
+     * @param  {String} data A text string to send to
+     *                       the server
+     * @throws {INVALID_STATE_ERR} If connection is not currently OPEN.
+     * @throws {SYNTAX_ERR} The data is a string that has unpaired surrogates.
+     * @return {void}
+     */
+    WindowSocket.prototype.send = function(data) {
+
+    };
+
+    /**
+     * Closes the WindowSocket connection or connection
+     * attempt, if any. If the connection is already
+     * CLOSED, this method does nothing.
+     * @return {void}
+     *
+     * @param  {Number} code   A numeric value indicating the status
+     *                         code explaining why the connection is
+     *                         being closed. If this parameter is not
+     *                         specified, a default value of 1000
+     *                         (indicating a normal "transaction complete"
+     *                         closure) is assumed. See the list of status
+     *                         codes on the CloseEvent page for
+     *                         permitted values.
+     * @param  {String} reason A human-readable string explaining why the
+     *                         connection is closing. This string must be
+     *                         no longer than 123 bytes of UTF-8 text
+     *                         (not characters).
+     * @throws {INVALID_ACCESS_ERR} If an invalid code was specified.
+     * @throws {SYNTAX_ERR} The reason string is too long or contains
+     *                      unpaired surrogates.
+     * @return {void}
+     */
+    WindowSocket.prototype.close = function(code, reason) {
+
     };
 
     /**
