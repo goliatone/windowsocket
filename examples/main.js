6471bf3ff9e72b53b94fe472bbfcddfb51e4d7f6
goliatone <burgosemi@gmail.com>
2014-10-02 23:10:09 -0400

    EXAMPLE: WIP testing API

--- examples/main.js
+++ examples/main.js
@@ -11,4 +11,13 @@ requirejs.config({
 define(['windowsocket', 'jquery'], function (WindowSocket, $) {
     console.log('Loading');
 	var windowsocket = new WindowSocket('tcp://localhost:9000/test');
+	windowsocket.send();
+	windowsocket.onopen = function(e){
+		console.log('WE ARE OPEN', e, windowsocket.readyState);
+	};
+
+	windowsocket.onerror = function(e){
+		console.error('WE HAVE ERROR', e);
+	};
+
 });
